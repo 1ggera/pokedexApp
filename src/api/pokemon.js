e154b5c499f229c17c96e25db0235c187e317fa1
Gerardo Garcia <ggeraweb@gmail.com>
2022-03-21 15:11:36 -0300

    enlace con api de pokemones

---
+++ src/api/pokemon.js
@@ -0,0 +1,12 @@
+import { API_HOST } from "../utils/constants"
+
+export async function getPokemonsApi(){
+  try{
+    const url = `${API_HOST}/pokemon?limit=20&offset=0`; //construye la url
+    const response = await fetch(url); //ejecuta la peticion al servidor
+    const result = await response.json(); //recupera los datos
+    return result;//devolvemos ese json con los  datos
+  } catch(error){
+    throw error;
+  }
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { getPokemonsApi } from "../api/pokemon"
 
 export default function Pokedex() {
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  //función q devuelve los datos de los pokemones
+  const loadPokemons = async () =>{
+    try{
+      const response = await getPokemonsApi();
+      console.log(response);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>Pokedex</Text>
-    </View>
+    </SafeAreaView>
   )
 }
