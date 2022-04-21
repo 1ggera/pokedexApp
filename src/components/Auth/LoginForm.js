996800a11d36f0a49b1c09bf4aecafd12885d1b0
gera garcia <ggeraweb@gmail.com>
2022-04-21 03:06:10 -0300

    config de formulario de registro

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native'
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm... </Text>
+      <Text style={styles.title}>Iniciar Sesion </Text>
+
+      <TextInput placeholder="Nombre de usuario" style={styles.input} autoCapitalize="none"></TextInput>
+
+      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize="none" secureTextEntry={true}>
+      </TextInput>
+
+      <Button title="Entrar" onPress={() => console.log("Ingresando...")}></Button>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title:{
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input:{
+    height: 40,
+    margin: 15,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+})
--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text} from 'react-native';
 import LoginForm from "../components/Auth/LoginForm";
-import UserPanel from '../components/Auth/UserPanel';
+import UserPanel from "../components/Auth/UserPanel";
 
 export default function Account() {
 
