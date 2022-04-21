d18f32bbff2b8bd1f551ef6dd1c5131cad6d0477
gera garcia <ggeraweb@gmail.com>
2022-04-21 02:06:56 -0300

    carga de panel de auth

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text} from 'react-native';
+import LoginForm from "../components/Auth/LoginForm";
+import UserPanel from '../components/Auth/UserPanel';
 
 export default function Account() {
+
+  const auth = null;
+  
   return (
     <View>
-      <Text>Account</Text>
+      {auth ? <UserPanel/> : <LoginForm/>}
     </View>
   )
 }
+
+/**
+ * 
+ * I am a man with big dreams in tecnology. Take mi CV and give me a chance.
+ * 
+ */
---
+++ src/utils/userDB.js
@@ -0,0 +1,14 @@
+//datos para ingresar
+export const user = {
+  username: "geranio92",
+  password: "123456",
+};
+
+//datos q devolverá si el login es correcto
+export const userDetails = {
+  username: "geranio92",
+  fristName: "Gerardo",
+  lastname: "Garcia",
+  email: "geranio92@gmail.com",
+};
+
