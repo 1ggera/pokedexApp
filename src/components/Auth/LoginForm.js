fd99438fe07329c6a321dabe95d40c15190bc4dd
gera garcia <ggeraweb@gmail.com>
2022-04-27 20:23:17 -0300

    cambios por fix en merge

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import {user, userDetaila} from "../../utils/userDB"
->>>>>>> Stashed changes
-=======
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import {user, userDetaila} from "../../utils/userDB"
->>>>>>> Stashed changes
+import {user, userDetails} from "../../utils/userDB"
 
 export default function LoginForm() {
   
@@ -28,19 +19,6 @@ export default function LoginForm() {
 
   return (
     <View>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <Text style={styles.title}>Iniciar Sesion </Text>
-
-      <TextInput placeholder="Nombre de usuario" style={styles.input} autoCapitalize="none"></TextInput>
-
-      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize="none" secureTextEntry={true}>
-      </TextInput>
-
-      <Button title="Entrar" onPress={() => console.log("Ingresando...")}></Button>
-=======
-=======
->>>>>>> Stashed changes
       <Text style={styles.title}>Iniciar sesión</Text>
       <TextInput
         placeholder="nombre_de_usuario"
@@ -55,23 +33,14 @@ export default function LoginForm() {
 
       <Text style={styles.error}>{formik.errors.username}</Text>
       <Text style={styles.error}>{formik.errors.password}</Text>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     </View>
   )
 }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 const styles = StyleSheet.create({
   title:{
     textAlign: "center",
     fontSize: 28,
-=======
-=======
->>>>>>> Stashed changes
 function initialValues (){
   return{
     username: "",
@@ -90,43 +59,24 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     fontSize: 32,
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     fontWeight: "bold",
     marginTop: 50,
     marginBottom: 15,
   },
   input:{
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     height: 40,
     margin: 15,
-=======
     height: 50,
     margin: 12,
->>>>>>> Stashed changes
-=======
     height: 50,
     margin: 12,
->>>>>>> Stashed changes
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
   },
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
   error:{
     textAlign: "center",
     color: "#f00",
     marginTop: 20,
   }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 })
