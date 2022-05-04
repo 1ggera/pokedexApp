ce157f0c7bc6e439870bde217b1845917f4d0dd2
gera garcia <ggeraweb@gmail.com>
2022-05-04 13:45:36 -0300

    add fav con AsyncStorage. Fix: sobreescribe el elemento ingresado

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import Icon from "react-native-vector-icons/FontAwesome5"
+import React from 'react';
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { addPokemonFavoriteApi, getPokemonsFavoriteApi } from "../../api/favorite"
 
 export default function Favorite(props) {
   const { id } = props; 
-  const addFavorite = () => {
-    console.log("Añadir a favoritos", id);
+  
+  const addFavorite = async () => {
+    await addPokemonFavoriteApi(id);
+  };
+
+  const getFav = async () => {
+    const response = await getPokemonsFavoriteApi(id);
+    console.log(response);
   }
+
   return (
-    <Icon name="heart" color="#fff" size={24} onPress={addFavorite} style={{ marginRight: 6 }}
-  />);
+    <>
+      <Icon name="heart" color="#fff" size={24} onPress={addFavorite} style={{ marginRight: 6 }}/>
+
+      <Icon name="user" color="#fff" size={24} onPress={getFav} style={{ marginRight: 6 }}/>
+    </>
+  );
 }
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,10 +1,26 @@
-import { SafeAreaView, View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, Button } from 'react-native'
+import { getPokemonsFavoriteApi } from "../api/favorite"
 
 export default function Favorite() {
+  const [favorites, setFavorites] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getPokemonsFavoriteApi();
+      console.log(response);
+    })();
+  });
+
+  // const chekFavorites = async() => {
+  //   const response = await getPokemonsFavoriteApi();
+  //   console.log(response);
+  // }
+
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
+      {/* <Button title="Obtener Favoritos" onPress={chekFavorites}/> */}
     </SafeAreaView>
   )
 }
