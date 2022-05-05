e361dc27b1a35becc58e1156259157f6a2502c1b
gera garcia <ggeraweb@gmail.com>
2022-05-05 15:29:04 -0300

    ahora guarda los pokemones en un array pero no sobreescribe los repetidos

--- src/api/favorite.js
+++ src/api/favorite.js
@@ -6,7 +6,7 @@ import { FAVORITE_STORAGE } from "../utils/constants"
 export async function getPokemonsFavoriteApi(){
   try{
     const response = await AsyncStorage.getItem(FAVORITE_STORAGE);
-    return response;
+    return JSON.parse(response || []);
   }catch (error){
     throw error;
   }
@@ -15,7 +15,7 @@ export async function getPokemonsFavoriteApi(){
 //añade elementos a favoritos
 export async function addPokemonFavoriteApi(id){
   try{
-    const favorites = [];
+    const favorites = await getPokemonsFavoriteApi();
     favorites.push(id);
     await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(favorites));
   } catch(error){
